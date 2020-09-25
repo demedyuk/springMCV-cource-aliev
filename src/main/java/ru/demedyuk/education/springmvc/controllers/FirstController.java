1277bb577549fd73fa15ec10cf8b54428e957cc2
Andrey Demedyuk <demedyuk@ngs.ru>
2020-09-25 23:39:57 +0700

    Lesson 17: Annotation @Controller, @RequestMapping

---
+++ src/main/java/ru/demedyuk/education/springmvc/controllers/FirstController.java
@@ -0,0 +1,21 @@
+package ru.demedyuk.education.springmvc.controllers;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/first")
+public class FirstController {
+
+    @GetMapping("/hello")
+    public String helloPage() {
+        return "first/hello";
+    }
+
+    @GetMapping("/goodbye")
+    public String goodbyePage() {
+        return "first/goodbye";
+    }
+}
